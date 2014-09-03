44dd912f17bb59e7562be25e4e4decb4fcbce0e3
Mary Le <maaaryle@users.noreply.github.com>
2014-09-03 14:01:56 -0700

    Update testone.js

--- testone.js
+++ testone.js
@@ -1,19 +1,19 @@
 $(document).ready(function(){
-	$('button').on('click', function(){
-		$.ajax({
-	  		url: "http://domains.bootname.com/",
-	  		dataType:"jasonp",
-	  		beforeSend: function() {
-			$('#pendingR').append("Checking domain availability").show();
-		  	}
-	  	})
-			.done(function(data) {
+	$('form').on('submit', function(){
+		$('#pendingR').append("Loading...").show();
+		var domain = $("#domain").val();
+		$.ajax("http://domains.bootname.co/api/v1/domain/" + domain, {
+	  		dataType: "jasonp",
+	  		success: function(data) {
 				$('#pendingR').hide();
-	  			if (data === "#domainname") {
+				var result = data[0];
+				if (result.available) {
 	  				$('#trueR').append("This domain is available").css({"text-color": "green"}).show();
 	  			} else {
 	  				$('#falseR').append("This domain is not available").css({"text-color": "red"}).show();
-	  		};
+	  			};	
+			}
 		});
+		return false;
 	});
 });
